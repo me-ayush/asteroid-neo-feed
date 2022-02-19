79b69463e6c46ed104927aaf872683235578a8ab
Ayush <ayushkushwaha321@gmail.com>
2022-02-19 12:58:57 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import ShowData from "./components/ShowData";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [start, setStart] = useState("");
+  const [end, setEnd] = useState("");
+  const [data, setData] = useState(null);
+  const [error, setError] = useState();
+  const [isLoading, setLoading] = useState("");
+
+  const check = (e) => {
+    setData(null);
+    e.preventDefault();
+    if (start == "") {
+      setError("Please Choose Start Date");
+    } else if (end == "") {
+      setError("Please Choose End Date");
+    } else if (end < start) {
+      setError("Start Date Should Be Before End Date");
+    } else {
+      var date1 = new Date(start);
+      var date2 = new Date(end);
+      var Difference_In_Time = date2.getTime() - date1.getTime();
+      var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+
+      if (Difference_In_Days > 7) {
+        setError("The Feed date limit is only 7 Days");
+      } else {
+        setLoading("Fetching Data...");
+        document.getElementById("loading").innerHTML = "Fetching Data...";
+        fetchData();
+      }
+    }
+  };
+
+  const fetchData = async () => {
+    setData(null);
+    getData().then((res) => setData(res), setError(false));
+  };
+
+  function getData() {
+    return axios({
+      url: `https://api.nasa.gov/neo/rest/v1/feed?start_date=${start}&end_date=${end}&api_key=JVRGRgawXWo22txapRAqh0MYhAqLiH3GtV7zUiz7`,
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.data)
+      .catch((err) => console.log(err));
+  }
+
+  const toSend = [data, start, end, isLoading];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1>Asteroid Neo Feed</h1>
+        <form onSubmit={check}>
+          <span>Start Date:</span>
+          <input
+            className="start-date"
+            type="date"
+            onChange={(e) => setStart(e.target.value)}
+          ></input>
+          <span>End Date:</span>
+          <input
+            className="end-date"
+            type="date"
+            onChange={(e) => setEnd(e.target.value)}
+          ></input>
+          <button className="btn btn-success btn-large" id="show-btn">
+            Submit
+          </button>
+        </form>
+      </div>
+      <div className="show-data">
+        <p id="loading">{isLoading}</p>
+        {data ? <ShowData data={toSend} /> : <p className="error">{error}</p>}
+      </div>
+    </>
   );
 }
 
---
+++ src/components/ShowData.js
@@ -0,0 +1,145 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import { useState, useEffect } from "react";
+import { Chart as ChartJS } from "chart.js/auto";
+
+function ShowData(props) {
+  const neo = props["data"][0]["near_earth_objects"];
+  const startDate = props["data"][1];
+  const endDate = props["data"][2];
+
+  function dateRange(startDate, endDate, steps = 1) {
+    const dateArray = [];
+    let currentDate = new Date(startDate);
+
+    while (currentDate <= new Date(endDate)) {
+      var today = new Date(currentDate);
+      var date = today.toJSON().slice(0, 10);
+      var nDate =
+        date.slice(0, 4) + "-" + date.slice(5, 7) + "-" + date.slice(8, 10);
+      dateArray.push(nDate);
+      currentDate.setUTCDate(currentDate.getUTCDate() + steps);
+    }
+    return dateArray;
+  }
+  const dates = dateRange(startDate, endDate);
+
+  const dates_obj = [];
+  for (var i in dates) {
+    let key = dates[i];
+    let fastest = 0;
+    let fastestName = "";
+    let c = 0;
+    let sizeMin = 0;
+    let sizeMax = 0;
+    let nearestDist = 1.797693134862315e308;
+    let nearestName = "";
+    for (var k1 in neo[key]) {
+      let currSpeed =
+        neo[key][k1]["close_approach_data"][0]["relative_velocity"][
+          "kilometers_per_hour"
+        ];
+      let currSizeMin =
+        neo[key][k1]["estimated_diameter"]["kilometers"][
+          "estimated_diameter_min"
+        ];
+      let currSizeMax =
+        neo[key][k1]["estimated_diameter"]["kilometers"][
+          "estimated_diameter_max"
+        ];
+      let currAstDist =
+        neo[key][k1]["close_approach_data"][0]["miss_distance"]["kilometers"];
+      if (currAstDist < nearestDist) {
+        nearestDist = currAstDist;
+        nearestName = neo[key][k1]["name"];
+      }
+      if (currSpeed > fastest) {
+        fastest = currSpeed;
+        fastestName = neo[key][k1]["name"];
+      }
+      sizeMin += currSizeMin;
+      sizeMax += currSizeMax;
+      c++;
+    }
+    sizeMax = sizeMax / c;
+    sizeMin = sizeMin / c;
+    dates_obj.push({
+      date: key,
+      ast: c,
+      fastest: fastest,
+      fastestName: fastestName,
+      avgMax: sizeMax,
+      avgMin: sizeMin,
+      nearest: nearestDist,
+      nearestName: nearestName,
+    });
+  }
+
+  const [chartData, setUserData] = useState({
+    labels: dates_obj.map((data) => data.date),
+    datasets: [
+      {
+        label: "Asteroid",
+        data: dates_obj.map((data) => data.ast),
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.6)",
+          "rgba(54, 162, 235, 0.6)",
+          "rgba(255, 206, 86, 0.6)",
+          "rgba(75, 192, 192, 0.6)",
+          "rgba(153, 102, 255, 0.6)",
+          "rgba(99, 252, 255, 0.6)",
+          "rgba(116, 255, 99, 0.6)",
+          "rgba(99, 111, 255, 0.6)",
+        ],
+        xAxisID: "Jello",
+        yAxisID: "ok",
+        borderColor: "black",
+        borderWidth: 1.5,
+      },
+    ],
+  });
+
+  useEffect(() => {
+    document.getElementById("loading").innerHTML = "";
+  });
+
+  return (
+    <>
+      <div className="table">
+        <table width={`100%`}>
+          <tr>
+            <th></th>
+            <th>Fastest Asteroid (Km/h)</th>
+            <th>Average Size (Km)</th>
+            <th>Nearest (Km)</th>
+          </tr>
+          {dates_obj.map((item, i) => (
+            <tr key={item.date}>
+              <td>{item.date}</td>
+              <td>
+                Name: {item.fastestName}
+                <br />
+                Speed: {item.fastest}
+              </td>
+              <td>
+                Max: {item.avgMax}
+                <br />
+                Min: {item.avgMin}
+              </td>
+              <td>
+                Name: {item.nearestName}
+                <br />
+                Distance: {item.nearest}
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
+      <div className="chart">
+        <Bar data={chartData} />
+      </div>
+    </>
+  );
+}
+
+export default ShowData;
